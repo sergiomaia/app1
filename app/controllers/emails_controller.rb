c65563d66b348dcc3fae0a7d033e60fd8f07163c
sergiomaia <sergiomaia@gmail.com>
2011-07-16 15:42:51 -0300

    implementation for app 2

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,10 +1,18 @@
+require "net/http"
+require "uri"
+
 class EmailsController < ApplicationController
+
   def new
 
   end
 
   def create
     email = params[:email]
+
+    uri = URI.parse("http://0.0.0.0:3001/emails")
+    response = Net::HTTP.post_form(uri, {"adress" => email})
+
     flash[:notice] = "Email recebido."
     render 'new'
   end
